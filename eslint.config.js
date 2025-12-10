4c4c248a0e97c878c5e9815e622279cbdc173c95
LSleutsky <LushSleutsky@gmail.com>
2025-12-09 21:58:09 -0500

    build: init commit and eslint/prettier config

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+import globals from "globals";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: ["**/*.{ts,tsx,js}"],
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      prettier: prettierPlugin
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      ...reactHooks.configs.recommended.rules,
+      "prettier/prettier": "error"
+    }
+  },
+  prettierConfig,
+  {
+    ignores: ["node_modules/", "build/", ".react-router/"]
+  }
+);
